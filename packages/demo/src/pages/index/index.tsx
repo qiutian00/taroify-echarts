c515fe667387f8f1d34d491417850e8539f2b572
qiutian00 <1476102147@qq.com>
2023-11-07 19:49:48 +0800

    init commit

---
+++ packages/demo/src/pages/index/index.tsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react'
+import Echarts, { EChartOption, EchartsHandle } from 'taroify-echarts'
+import echarts from '../../assets/js/echarts'
+import './index.less'
+
+export default function Demo() {
+  const echartsRef = useRef<EchartsHandle>(null)
+  const option: EChartOption = {
+    legend: {
+      top: 50,
+      left: 'center',
+      z: 100,
+    },
+    tooltip: {
+      trigger: 'axis',
+      show: true,
+      confine: true,
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line',
+      },
+    ],
+  }
+
+  return <Echarts echarts={echarts} option={option} ref={echartsRef} />
+}
---
+++ packages/echarts/src/index.ts
@@ -0,0 +1,11 @@
+import Echarts from './core'
+
+export type {
+  EChartsProps,
+  EChartOption,
+  EChartsInstance,
+  EChartsLoadingOption,
+  EchartsHandle,
+} from './core/types'
+
+export default Echarts
